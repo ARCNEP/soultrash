65b2035cdf71aee503324983411f8e1758ba7ff6
ARCNEP <45639959+ARCNEP@users.noreply.github.com>
2019-01-24 19:56:07 -0700

    Create audio-hover.js

---
+++ audio-hover.js
@@ -0,0 +1,88 @@
+	var $song1 = $('#song1');
+	var $audio1 = $('#audio1');
+
+	$audio1.on('click', function (){
+		$song1.get(0).play();
+
+	});
+
+	$audio1.on('dblclick', function (){
+		$song1.get(0).load();
+
+	});
+
+var $song2 = $('#song2');
+	var $audio2 = $('#audio2');
+
+	$audio2.on('click', function (){
+		$song2.get(0).play();
+
+
+
+	});
+
+	$audio2.on('dblclick', function (){
+		$song2.get(0).load();
+
+	});
+
+var $song3 = $('#song3');
+	var $audio3 = $('#audio3');
+
+	$audio3.on('click', function (){
+		$song3.get(0).play();
+
+	});
+
+	$audio3.on('dblclick', function (){
+		$song3.get(0).load();
+
+	});
+
+var $song4 = $('#song4');
+	var $audio4 = $('#audio4');
+
+	$audio4.on('click', function (){
+		$song4.get(0).play();
+
+	});
+
+	$audio4.on('dblclick', function (){
+		$song4.get(0).load();
+
+	});
+
+	var $song5 = $('#song5');
+	var $audio5 = $('#audio5');
+
+	$audio5.on('click', function (){
+		$song5.get(0).play();
+
+	});
+
+	$audio5.on('dblclick', function (){
+		$song5.get(0).load();
+
+	});
+
+var $song6 = $('#song6');
+	var $audio6 = $('#audio6');
+
+	$audio6.on('click', function (){
+		$song6.get(0).play();
+
+	});
+
+	$audio6.on('dblclick', function (){
+		$song6.get(0).load();
+
+	});
+
+
+
+
+
+
+
+
+
